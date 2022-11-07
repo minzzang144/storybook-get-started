4a7ea8cec1b13fc0d4412dd6af63321ef55326d3
ShigatsuEl <shigatsu970704@gmail.com>
2022-11-07 22:48:06 +0900

    feat(design-systems-for-developers): 빌드

--- packages/design-systems-for-developers/src/Avatar.stories.js
+++ packages/design-systems-for-developers/src/Avatar.stories.js
@@ -3,59 +3,81 @@ import React from "react";
 import { Avatar } from "./Avatar";
 
 export default {
-  title: "Design System/Avatar",
-  component: Avatar,
+    title: "Design System/Avatar",
+    component: Avatar,
+    argTypes: {
+        size: {
+            control: {
+                type: "select",
+            },
+            options: ["tiny", "small", "medium", "large"],
+        },
+    },
 };
 
 export const Standard = (args) => <Avatar {...args} />;
 Standard.args = {
-  size: "large",
-  username: "Tom Coleman",
-  src: "https://avatars2.githubusercontent.com/u/132554",
+    size: "large",
+    username: "Tom Coleman",
+    src: "https://avatars2.githubusercontent.com/u/132554",
 };
 
 export const Sizes = (args) => (
-  <div>
-    <Avatar {...args} size="large" />
-    <Avatar {...args} size="medium" />
-    <Avatar {...args} size="small" />
-    <Avatar {...args} size="tiny" />
-  </div>
+    <div>
+        <Avatar {...args} size="large" />
+        <Avatar {...args} size="medium" />
+        <Avatar {...args} size="small" />
+        <Avatar {...args} size="tiny" />
+    </div>
 );
 Sizes.args = {
-  username: "Tom Coleman",
-  src: "https://avatars2.githubusercontent.com/u/132554",
+    username: "Tom Coleman",
+    src: "https://avatars2.githubusercontent.com/u/132554",
 };
 
 export const Initials = (args) => (
-  <div>
-    <Avatar username="Tom Coleman" />
-    <Avatar username="Dominic Nguyen" />
-    <Avatar username="Kyle Suss" />
-    <Avatar username="Michael Shilman" />
-  </div>
+    <div>
+        <Avatar username="Tom Coleman" />
+        <Avatar username="Dominic Nguyen" />
+        <Avatar username="Kyle Suss" />
+        <Avatar username="Michael Shilman" />
+    </div>
 );
 
 export const Loading = (args) => (
     <div>
-      <Avatar {...args} size="large" />
-      <Avatar {...args} size="medium" />
-      <Avatar {...args} size="small" />
-      <Avatar {...args} size="tiny" />
+        <Avatar {...args} size="large" />
+        <Avatar {...args} size="medium" />
+        <Avatar {...args} size="small" />
+        <Avatar {...args} size="tiny" />
     </div>
-  );
-  Loading.args = {
+);
+Loading.args = {
     loading: true,
-  };
+};
 
 export const Large = (args) => (
-  <div>
-    <Avatar loading size="large" />
-    <Avatar size="large" username="Tom Coleman" />
-    <Avatar
-      size="large"
-      username="Tom Coleman"
-      src="https://avatars2.githubusercontent.com/u/132554"
-    />
-  </div>
+    <div>
+        <Avatar loading size="large" />
+        <Avatar size="large" username="Tom Coleman" />
+        <Avatar
+            size="large"
+            username="Tom Coleman"
+            src="https://avatars2.githubusercontent.com/u/132554"
+        />
+    </div>
 );
+
+const Template = (args) => <Avatar {...args} />;
+
+export const Controls = Template.bind({});
+/*
+ * More on args at:
+ * https://storybook.js.org/docs/react/writing-stories/args
+ */
+Controls.args = {
+    loading: false,
+    size: "tiny",
+    username: "Dominic Nguyen",
+    src: "https://avatars2.githubusercontent.com/u/263385",
+};
